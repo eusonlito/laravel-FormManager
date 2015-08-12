666e870d22eb7f5702914d8263be738bfd877cc0
Lito <lito@eordes.com>
2015-08-13 00:13:08 +0200

    Added Select with content related

--- src/Renders/Bootstrap.php
+++ src/Renders/Bootstrap.php
@@ -26,6 +26,28 @@ class Bootstrap extends Render
             .'</label></div></div>';
     }
 
+    public function select($input)
+    {
+        if (!($related = $input->data('related'))) {
+            return $this->basic($input);
+        }
+
+        $input->addClass('form-control');
+
+        $this->visualRequired($input);
+
+        $input->data('related', false);
+
+        $html = $this->label($input)
+            .'<div class="input-group form-group" data-related>'
+            .$input->input;
+
+        return $html.'<span class="input-group-btn">'
+            .'<a href="'.$related.'" class="btn btn-primary">'
+            .'<i class="glyphicon glyphicon glyphicon-share-alt"></i>'
+            .'</a></span></div>';
+    }
+
     public function file($input)
     {
         $input->addClass('form-control');
